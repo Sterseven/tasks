3e846e66021f9b51adc7fe6fd487157dd0a293f9
Sterseven <55618567+Sterseven@users.noreply.github.com>
2019-12-22 18:32:27 +0300

    Update task4.py

--- task4/task4.py
+++ task4/task4.py
@@ -8,56 +8,67 @@ with open(filee) as f:
     for line in f:
         list1.append(line)
 
-clist = []
+if len(list1) == 0:
+    print("No visitors")
 
-for elem in list1:
-    clist.append(elem[:-1])
+else:
 
-rep = []
+    clist = []
 
-for elem in clist:
-    rep.append(elem.replace(":", "."))
+    for elem in list1:
+        clist.append(elem[:-1])
+
+    rep = []
+
+    for elem in clist:
+        rep.append(elem.replace(":", "."))
     
-intone = []
-intsec = []
+    intone = []
+    intsec = []
 
-for elem in rep:
-    intone.append(float(elem.split(' ')[0]))
-    intsec.append(float(elem.split(' ')[1]))
+    for elem in rep:
+        intone.append(float(elem.split(' ')[0]))
+        intsec.append(float(elem.split(' ')[1]))
 
-inter = set()
-t = []
-for x in range(len(intone)):
-    r = []
-    inter.add(intone[x])
-    inter.add(intsec[x])
-    r.append(intone[x])
-    r.append(intsec[x])
-    t.append(r)
+    inter = set()
+    t = []
+    for x in range(len(intone)):
+        r = []
+        inter.add(intone[x])
+        inter.add(intsec[x])
+        r.append(intone[x])
+        r.append(intsec[x])
+        t.append(r)
 
-inter = list(inter)
-inter.sort()
+    inter = list(inter)
+    inter.sort()
 
 
-vis = []
 
-d = {}
+
+    vis = []
+
+    d = {}
 
     
-
-for elem in range(len(inter) - 1):
-    vis.append(0)
-    for x in range(len(t)):
-        if t[x][0] <= inter[elem] < inter[elem + 1] <= t[x][1]:
-            vis[elem] += 1
-bol = max(vis)
+    
 
 
-for x in range(len(inter) - 1):
-    if bol == vis[x] and (x == 0 or bol != vis[x - 1]):
-        show = '{:.2f}'.format(inter[x]).replace('.', ':')
-        print(show, end=' ')
-        
-    if bol != vis[x] and bol == vis[x - 1]:
-        show = '{:.2f}'.format(inter[x]).replace('.', ':')
-        print(show)
+    for elem in range(len(inter) - 1):
+        vis.append(0)
+        for x in range(len(t)):
+            if t[x][0] <= inter[elem] < inter[elem + 1] <= t[x][1]:
+                vis[elem] += 1
+    bol = max(vis)
+
+
+    for x in range(len(inter) - 1):
+        if bol == vis[x] and (x == 0 or bol != vis[x - 1]):
+            show = '{:.2f}'.format(inter[x]).replace('.', ':')
+            print(show, end=' ')
+        if bol != vis[x] and bol == vis[x - 1]:
+            show = '{:.2f}'.format(inter[x]).replace('.', ':')
+            print(show)
+
+                  
+                  
